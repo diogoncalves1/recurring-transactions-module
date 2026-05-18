5895cbc146618a2197b50cd4f4419bcf928bc8a1
diogoncalves1 <dmgdark95@gmail.com>
2026-05-18 08:45:12 +0100

    feat(recurring transactions): Recurring Transaction Modules base

---
+++ Modules/RecurringTransaction/database/seeders/RecurringTransactionDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\RecurringTransaction\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RecurringTransactionDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
